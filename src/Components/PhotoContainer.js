4af9dea8cee88ff0faf4d9ac9700d93e9f09beb9
micahthobbs <39948231+micahthobbs@users.noreply.github.com>
2019-12-05 05:03:32 +1100

    Created photo container

---
+++ src/Components/PhotoContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function PhotoContainer(photos) {
+    return(
+        <div className="photo-container">
+            <h2>Results</h2>
+            <ul>
+                {
+
+                }
+            </ul>
+        </div>
+    ) 
+}
+
+export default PhotoContainer;
